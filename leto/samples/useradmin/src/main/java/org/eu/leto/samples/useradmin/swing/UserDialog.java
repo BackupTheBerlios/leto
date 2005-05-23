38095577003da4ad029dc23e825e5cd02e92a191
romale <romale>
2005-05-23 16:31:31 +0000

    fixed createContent

--- leto/samples/useradmin/src/main/java/org/eu/leto/samples/useradmin/swing/UserDialog.java
+++ leto/samples/useradmin/src/main/java/org/eu/leto/samples/useradmin/swing/UserDialog.java
@@ -1,12 +1,12 @@
 package org.eu.leto.samples.useradmin.swing;
 
 
-import java.awt.BorderLayout;
 import java.awt.Frame;
 
 import javax.swing.JComponent;
-import javax.swing.JPanel;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.eu.leto.core.application.Application;
 import org.eu.leto.form.FormModel;
 import org.eu.leto.samples.useradmin.UserForm;
@@ -14,25 +14,28 @@ import org.eu.leto.swing.AbstractExtendedDialog;
 
 
 public class UserDialog extends AbstractExtendedDialog {
-    private final FormModel userFormModel;
-    private JComponent userPanel;
+    private final Log log = LogFactory.getLog(getClass());
+    private UserForm userForm;
 
 
     public UserDialog(final Application application, final Frame owner) {
         super(application, owner, "dialog.user");
-        this.userFormModel = new FormModel<UserForm>(getApplication(), "userForm",
-                new UserForm());
         setResizable(false);
-        
-        userPanel = new UserFormBuilder(userFormModel).createControl();
     }
 
 
     @Override
     protected JComponent createContent() {
-        final JPanel panel = new JPanel(new BorderLayout());
-        panel.add(userPanel, BorderLayout.CENTER);
+        final FormModel userFormModel = new FormModel<UserForm>(
+                getApplication(), "userForm", userForm = new UserForm());
+        return new UserFormBuilder(userFormModel).createComponent();
+    }
 
-        return panel;
+
+    @Override
+    protected void onOk() {
+        if (log.isDebugEnabled()) {
+            log.debug("Validating dialog with user: " + userForm.getUser());
+        }
     }
 }
