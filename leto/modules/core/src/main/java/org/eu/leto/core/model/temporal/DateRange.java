e8d802b40d2d883ca1ed3a268024f9ada6693429
romale <romale>
2005-05-26 16:15:16 +0000

    refactored out from the new generic AbstractRange

--- leto/modules/core/src/main/java/org/eu/leto/core/model/temporal/DateRange.java
+++ leto/modules/core/src/main/java/org/eu/leto/core/model/temporal/DateRange.java
@@ -1,27 +1,15 @@
-
-
 package org.eu.leto.core.model.temporal;
 
-import java.io.Serializable;
-import java.util.Date;
 
-import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
-import org.apache.commons.lang.NullArgumentException;
-import org.apache.commons.lang.builder.CompareToBuilder;
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
-import org.apache.commons.lang.builder.ToStringBuilder;
+import org.eu.leto.core.model.AbstractRange;
 
 
 @Embeddable
-public class DateRange implements Comparable, Serializable {
-    private Date end;
-    private Date start;
-
+public class DateRange extends AbstractRange<Date> {
     public DateRange() {
-        this.start = this.end = new Date();
+        super();
     }
 
 
@@ -29,66 +17,5 @@ public class DateRange implements Comparable, Serializable {
         this();
         setStart(start);
         setEnd(end);
-    }
-
-    public void setEnd(Date end) {
-        if (end == null) {
-            throw new NullArgumentException("end");
-        }
-        this.end = end;
-    }
-
-
-    @Column(nullable = false)
-    public Date getEnd() {
-        return end;
-    }
-
-
-    public void setStart(Date start) {
-        if (start == null) {
-            throw new NullArgumentException("start");
-        }
-        this.start = start;
-    }
-
-
-    @Column(nullable = false)
-    public Date getStart() {
-        return start;
-    }
-
-
-    public int compareTo(Object obj) {
-        return CompareToBuilder.reflectionCompare(this, obj);
-    }
-
-
-    public boolean equals(Object obj) {
-        return EqualsBuilder.reflectionEquals(this, obj);
-    }
-
-
-    public int hashCode() {
-        return HashCodeBuilder.reflectionHashCode(this);
-    }
-
-
-    public boolean includes(DateRange range) {
-        return includes(range.getStart()) && includes(range.getEnd());
-    }
-
-
-    public boolean includes(Date date) {
-        if (date == null) {
-            throw new NullArgumentException("date");
-        }
-
-        return !date.before(start) && !date.after(end);
-    }
-
-
-    public String toString() {
-        return ToStringBuilder.reflectionToString(this);
     }
 }
