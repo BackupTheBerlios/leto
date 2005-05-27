56939675e9f980ab85af551a60716d4ec580d52a
romale <romale>
2005-05-27 08:22:18 +0000

    initial checkin

---
+++ leto/modules/core/src/main/java/org/eu/leto/core/application/DefaultApplication.java
@@ -0,0 +1,28 @@
+package org.eu.leto.core.application;
+
+
+public class DefaultApplication extends AbstractApplication {
+    public DefaultApplication(final String id) {
+        super(id);
+    }
+
+
+    @Override
+    protected void onDispose() throws Exception {
+    }
+
+
+    @Override
+    protected void onInit() throws Exception {
+    }
+
+
+    @Override
+    protected void onStart() throws Exception {
+    }
+
+
+    @Override
+    protected void onStop() throws Exception {
+    }
+}
